879b9f374c6f60c31ac49e2dc77e51acc6d67ee7
privalk <87178164+privalk@users.noreply.github.com>
2025-07-03 15:43:17 +0800

    Improve Canon EOS 1500D support

--- core/src/TethrPTPUSB/TethrCanon.ts
+++ core/src/TethrPTPUSB/TethrCanon.ts
@@ -1,4 +1,12 @@
+import {PTPDevice} from '../PTPDevice'
 import {TethrPTPUSB} from './TethrPTPUSB'
+
+enum OpCodeCanon {
+    SetRemoteMode = 0x9114,
+    SetEventMode = 0x9115,
+    RemoteReleaseOn = 0x9153,
+    RemoteReleaseOff = 0x9154,
+}
 
 /**
  * Experimental Canon EOS support. Currently no vendor specific
@@ -6,5 +14,51 @@ import {TethrPTPUSB} from './TethrPTPUSB'
  * easier to add them later.
  */
 export class TethrCanon extends TethrPTPUSB {
-        // Canon specific implementations will be added here in the future
+    constructor(device: PTPDevice) {
+        super(device)
+    }
+
+    async open(): Promise<void> {
+        await super.open()
+
+        await this.device.sendCommand({
+            label: 'Canon SetRemoteMode',
+            opcode: OpCodeCanon.SetRemoteMode,
+            parameters: [1],
+        })
+
+        await this.device.sendCommand({
+            label: 'Canon SetEventMode',
+            opcode: OpCodeCanon.SetEventMode,
+            parameters: [1],
+        })
+    }
+
+    async close(): Promise<void> {
+        await this.device.sendCommand({
+            label: 'Canon SetEventMode Off',
+            opcode: OpCodeCanon.SetEventMode,
+            parameters: [0],
+        })
+
+        await this.device.sendCommand({
+            label: 'Canon SetRemoteMode Off',
+            opcode: OpCodeCanon.SetRemoteMode,
+            parameters: [0],
+        })
+
+        await super.close()
+    }
+
+    async remoteRelease(): Promise<void> {
+        await this.device.sendCommand({
+            label: 'Canon RemoteReleaseOn',
+            opcode: OpCodeCanon.RemoteReleaseOn,
+        })
+
+        await this.device.sendCommand({
+            label: 'Canon RemoteReleaseOff',
+            opcode: OpCodeCanon.RemoteReleaseOff,
+        })
+    }
 }
